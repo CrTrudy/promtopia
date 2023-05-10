a422d69f208476c95ee1760abd90bf0906721e4e
CrTrudy <111459119+CrTrudy@users.noreply.github.com>
2023-05-10 23:18:21 +0200

    ++

---
+++ components/PromptCard.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from 'react';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import { usePathname, useRoute } from 'next/navigation';
+
+const PromptCard = () => {
+    return (
+        <div>
+            PromptCard
+        </div>
+    )
+}
